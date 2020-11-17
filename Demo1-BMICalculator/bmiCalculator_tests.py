d62ef737abc678487c83af80424f5653b0ba688f
dcook-net <david.cook@bglgroup.co.uk>
2020-11-17 17:17:15 +0000

    update slides.

--- Demo1-BMICalculator/bmiCalculator_tests.py
+++ Demo1-BMICalculator/bmiCalculator_tests.py
@@ -1,7 +1,6 @@
 import pytest
 from bmi import BodyMassIndex
 
-
 # Problem/Requirement:
 
 # Calculate BMI (Body Mass Index) to 2 decimal places using the formula:
@@ -13,23 +12,14 @@ from bmi import BodyMassIndex
 # | 84 kg  | 174 cm | 27.74        |
 # | 64 kg  | 194 cm | 17.00        |
 
-# @pytest.mark.parametrize('height, weight , expected_bmi', [
-#     (184, 74, 21.86),
-#     (174, 84, 27.74),
-#     (194, 64, 17.00)
-# ])
-# def test_calculate_bmi(height, weight, expected_bmi):
-#     # ACT
-#     actual_bmi = BodyMassIndex.calculate(height, weight)
-#
-#     # ASSERT
-#     assert actual_bmi == expected_bmi
 
-
-@pytest.mark.parametrize('height, weight , expected_bmi', [(184, 74, 21.86), (174, 84, 27.74), (194, 64, 17.00)])
-def test_calculate_bmi(height, weight, expected_bmi):
+@pytest.mark.parametrize('height_cm, weight_kg, expected_bmi', [
+    (184, 74, 21.86),
+    (174, 84, 27.74),
+    (194, 64, 17.00)])
+def test_calculate_bmi(height_cm, weight_kg, expected_bmi):
     # ACT
-    actual_bmi = BodyMassIndex.calculate(height, weight)
+    actual_bmi = BodyMassIndex.calculate(height_cm, weight_kg)
 
     # ASSERT
     assert actual_bmi == expected_bmi
